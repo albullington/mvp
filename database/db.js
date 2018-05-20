63edfb13e81f53e46c425e0e896275e2def3225c
Amanda <albullington@gmail.com>
2018-05-19 17:22:52 -0700

    seeded database with 10 examples

--- database/db.js
+++ database/db.js
@@ -1,10 +1,46 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-MongoClient.connect('mongodb://localhost:27017/glassdoor', (err, db) => {
-  if (!err) {
-    console.log('mongodb connected');
-  }
+mongoose.connect('mongodb://localhost:27017/glassdoor')
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    throw err;
+  });
 
-  const collection = db.collection('companies');
-
+const reviewSchema = new mongoose.Schema({
+  company: String,
+  industry: String,
+  jobTitle: String,
+  location: String,
+  leadershipLevel: String,
+  support: Boolean,
+  equality: Boolean,
+  voice: Boolean,
+  interruptions: Boolean,
+  compensation: {
+    salary: Number,
+    averageRaise: Number,
+    equity: Number,
+    numPromotions: Number,
+    maternityLeave: Number,
+    vacationDays: Number,
+    flexDays: Number,
+  },
+  nursingRoom: Boolean,
+  psychologicalSafety: Number,
+  recommendToWomen: Number,
+  comments: String,
 });
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports.searchReviews = (query) => {
+  Review.find(query);
+};
+
+module.exports.saveReviews = (doc) => {
+  Review.create(doc);
+};
+
+module.exports.Review = Review;
---
+++ database/seed-data.js
@@ -0,0 +1,40 @@
+const faker = require('faker');
+const mongoose = require('mongoose');
+const { Review } = require('./db');
+
+const examples = [];
+
+for (let i = 0; i < 10; i++) {
+  examples.push({
+    company: faker.company.companyName(),
+    industry: faker.name.jobArea(),
+    jobTitle: faker.name.jobTitle(),
+    location: faker.address.city(),
+    leadershipLevel: faker.name.title(),
+    support: faker.random.boolean(),
+    equality: faker.random.boolean(),
+    voice: faker.random.boolean(),
+    interruptions: faker.random.boolean(),
+    compensation: {
+      salary: faker.random.number(),
+      averageRaise: faker.random.number(),
+      equity: faker.random.number(),
+      numPromotions: faker.random.number(),
+      maternityLeave: faker.random.number(),
+      vacationDays: faker.random.number(),
+      flexDays: faker.random.number(),
+    },
+    nursingRoom: faker.random.boolean(),
+    psychologicalSafety: faker.random.number(),
+    recommendToWomen: faker.random.number(),
+    comments: faker.lorem.paragraph(),
+  });
+}
+
+Review.insertMany(examples)
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch((err) => {
+    throw err;
+  });
