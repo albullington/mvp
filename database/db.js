faba79517f3bc9b8465a5fb1d24ffe198675eb89
Amanda <albullington@gmail.com>
2018-05-21 15:41:14 -0700

    post request to save entries in database working

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/glassdoor')
-  .then((res) => {
-    console.log(res);
+  .then(() => {
+    console.log('mongoose connected');
   })
   .catch((err) => {
+    mongoose.disconnect();
     throw err;
   });
 
@@ -12,35 +13,35 @@ const reviewSchema = new mongoose.Schema({
   company: String,
   industry: String,
   jobTitle: String,
-  location: String,
   leadershipLevel: String,
-  support: Boolean,
-  equality: Boolean,
-  voice: Boolean,
   interruptions: Boolean,
-  compensation: {
-    salary: Number,
-    averageRaise: Number,
-    equity: Number,
-    numPromotions: Number,
-    maternityLeave: Number,
-    vacationDays: Number,
-    flexDays: Number,
-  },
-  nursingRoom: Boolean,
-  psychologicalSafety: Number,
-  recommendToWomen: Number,
-  comments: String,
+  recommend: Boolean,
 });
 
-const Review = mongoose.model('Review', reviewSchema);
+// const reviewSchema = new mongoose.Schema({
+//   company: String,
+//   industry: String,
+//   jobTitle: String,
+//   location: String,
+//   leadershipLevel: String,
+//   support: Boolean,
+//   equality: Boolean,
+//   voice: Boolean,
+//   interruptions: Boolean,
+//   compensation: {
+//     salary: Number,
+//     averageRaise: Number,
+//     equity: Number,
+//     numPromotions: Number,
+//     maternityLeave: Number,
+//     vacationDays: Number,
+//     flexDays: Number,
+//   },
+//   nursingRoom: Boolean,
+//   psychologicalSafety: Number,
+//   recommend: Number,
+//   comments: String,
+// });
 
-module.exports.searchReviews = (query) => {
-  Review.find(query);
-};
+module.exports.Review = mongoose.model('Review', reviewSchema);
 
-module.exports.saveReviews = (doc) => {
-  Review.create(doc);
-};
-
-module.exports.Review = Review;
