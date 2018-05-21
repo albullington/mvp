82c3edff5730676059c0ce3ca2b6e26dbcf3b126
Amanda <albullington@gmail.com>
2018-05-21 11:56:39 -0700

    form completed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import TopBar from './TopBar';
 import SearchBar from './SearchBar';
 import BestList from './BestList';
-import Submit from './Submit';
+import Form from './Form';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      leadershipLevel: 'entry-level',
+      interruption: true,
+      numberOfGuests: 2,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(`Your favorite flavor is: + ${this.state.value}`);
+    event.preventDefault();
   }
 
   render() {
@@ -17,7 +39,14 @@ export default class App extends Component {
         <TopBar />
         <SearchBar />
         <BestList />
-        <Submit />
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          leadershipLevel={this.state.leadershipLevel}
+          recommend={this.state.recommend}
+          interruption={this.state.interruption}
+          numberOfGuests={this.state.numberOfGuests}
+        />
       </div>
     );
   }
--- client/src/components/Submit.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Submit = () => (
-  <form>
-    <input placeholder="Enter your anonymous feedback" />
-  </form>
-);
-
-export default Submit;
